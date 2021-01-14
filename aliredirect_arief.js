81774b57dccfba4a7639f0ed2a612b87c051726b
sarjanamalas <77425955+sarjanamalas@users.noreply.github.com>
2021-01-14 12:09:26 +0700

    Update aliredirect_arief.js

--- aliredirect_arief.js
+++ aliredirect_arief.js
@@ -1,16 +1,21 @@
-var urlmain = document.URL;
-var md = document.URL.split("/")[3];
-var key = document.URL.split("/")[4];
-var id = document.URL.split("/")[5];
+// simple redirect by sarjanamalas
+// slugs setting disini
+// affkey flexible pasang di url, bukan script. ( generate di halaman portals.aliexpress.com )
 
-if (md == 'deep') {
-  var redir = 'http://s.click.aliexpress.com/deep_link.htm?aff_short_key='+key+'&dl_target_url=https://www.aliexpress.com/item/'+id+'';
-} else {
-  var redir = 'https://s.click.aliexpress.com/e/'+key;
-}
+// base url ->  https://domain.blogspot.com/slugs/kodeaffiliate/idproduk.html
+// contoh -> https://domain.blogspot.com/offers/_afk09s/40080090011.html
+
+var urlmain = document.URL;
+var slugs = document.URL.split("/")[3];
+var affkey = document.URL.split("/")[4];
+var idproduct = document.URL.split("/")[5];
+
+if (slugs == 'offers' OR 'promos' OR 'discount' OR 'superdeal' ) {
+  var redir = 'http://s.click.aliexpress.com/deep_link.htm?aff_short_key='+affkey+'&dl_target_url=https://www.aliexpress.com/item/'+idproduct+'';
+} else {}
 
 setTimeout(
   function(){
     window.location = redir
   },
-2000); 
+3000); 
