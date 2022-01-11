99b1a2b7b53030a6f45e940dc69d4d484b186b03
sarjanamalas <77425955+sarjanamalas@users.noreply.github.com>
2022-01-11 16:42:47 +0700

    Create sm.js

---
+++ sm.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '0c5676d42b74b6116be2916e378556db',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivedisplaycontent.com/0c5676d42b74b6116be2916e378556db/invoke.js"></scr' + 'ipt>');
