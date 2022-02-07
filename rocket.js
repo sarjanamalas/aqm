b22d4aab50a650e9a323763100ca90b060e24e03
sarjanamalas <77425955+sarjanamalas@users.noreply.github.com>
2022-02-07 22:26:34 +0700

    Update rocket.js

--- rocket.js
+++ rocket.js
@@ -3,7 +3,7 @@ var md = document.URL.split("/")[3];
 var key = document.URL.split("/")[4];
 var id = document.URL.split("/")[5];
 
-var redir = 'http://s.click.aliexpress.com/deep_link.htm?aff_short_key='+key+'&dl_target_url=https://www.aliexpress.com/item/'+id+'';
+var redir = 'http://s.click.aliexpress.com/deep_link.htm?aff_short_key='+key+'&dp='+md+'&dl_target_url=https://www.aliexpress.com/item/'+id+'';
 
 setTimeout(
   function(){
